80811aa7802118d9e142167ba1554ad6cb53d1c6
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2020-01-31 23:01:12 -0800

    got basic homepage endpoint working

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
-import flask restful
+from flask_restful import Api
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+@app.route("/", methods=['GET'])
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
