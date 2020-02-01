54d75928223bde76f166505917e62391c3951d64
Garimella, Sumedh Surya <sgarimella30@gatech.edu>
2020-02-01 02:16:29 -0800

    update main and requirements

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from flask_restful import Api
-from flask_cors import CORS
+import flask_restful
+import flask_cors
 
 app = Flask(__name__)
-CORS(app)
-api = Api(app)
+flask_cors.CORS(app)
+api = flask_restful.Api(app)
 
 @app.route("/", methods=['GET'])
 def home():
